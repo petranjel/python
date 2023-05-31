4f6e9770dad7d252941b9fb1b59a864af2770fd5
petranjel <petranjel@gmail.com>
2023-05-31 23:10:45 +0200

    Add to github

--- simple_treeview_posgeSQL.py
+++ simple_treeview_posgeSQL.py
@@ -2,12 +2,65 @@ import psycopg2
 import tkinter as tk
 from tkinter import ttk
 
+main_color = "#bffcce"
+db_table='pokladna'
+db_columns='description,value_data,c_time,row_id'
+connection_string=()
+           
+def config():
+    def set_server_config():
+        global connection_string
+        host='host='+"'"+entry_host.get()+"' "
+        dbname='dbname='+"'"+entry_dbname.get()+"' "
+        user='user='+"'"+entry_user.get()+"' "
+        password='password='+"'"+entry_password.get()+"' "
+        config_frame.destroy()
+        value=host+dbname+user+password
+        connection_string=value
+        with open('server_config.txt','w',encoding='utf-8') as write_cfg:
+            write_cfg.write(value)
+    config_frame=tk.Tk()
+    config_frame.geometry ('600x400')
+    config_frame.config (background=main_color)
+    config_frame.title ('server_config')
+    label_host=tk.Label(config_frame,text='Host:')
+    label_host.grid(column=1,row=1)
+    label_dbname=tk.Label(config_frame,text='Database name:')
+    label_dbname.grid(column=1,row=2)
+    label_user=tk.Label(config_frame,text='User:')
+    label_user.grid(column=1,row=3)
+    label_password=tk.Label(config_frame,text='Password:')
+    label_password.grid(column=1,row=4)
+    entry_host=tk.Entry(config_frame)
+    entry_host.grid(column=2,row=1)
+    entry_host.insert(0,'localhost')
+    entry_dbname=tk.Entry(config_frame)
+    entry_dbname.grid(column=2,row=2)
+    entry_dbname.insert(0,'name')
+    entry_user=tk.Entry(config_frame)
+    entry_user.grid(column=2,row=3)
+    entry_user.insert(0,'postgres')
+    entry_password=tk.Entry(config_frame)
+    entry_password.grid(column=2,row=4)
+    entry_password.insert(0,'password')
+    ok_button=tk.Button(config_frame,text='OK',command=set_server_config)
+    ok_button.grid(column=2,row=5)
+    config_frame.mainloop()
+
+try: #apent connecting string from text document
+    with open('server_config.txt','r',encoding='utf-8') as read_cfg:
+        for value in read_cfg:
+            connection_string=value    
+except: print('open connection string error')
+if connection_string == ():
+    config()
+else: pass
+
+
 frame_main = tk.Tk ()
 frame_main.geometry ('600x400')
-frame_main.config (background="#bffcce")
-frame_main.title ('name_in_title')
-
-main_color = "#bffcce"
+frame_main.config (background=main_color)
+frame_main.title ('in_title')
 
 frame_one=tk.Frame (frame_main,background=main_color)
 frame_two=tk.Frame (frame_main,background=main_color)
@@ -16,120 +69,91 @@ frame_one.pack ()
 frame_two.pack ()
 frame_three.pack ()
 
-def add_event ():
+def link_up(): # Connect to your database and create cursor
+    global conn,cursor
+    conn=psycopg2.connect(connection_string)
+    cursor=conn.cursor()
+def link_down(): # Close connection and cursor - must by after any operation with dababase
+    conn.close()
+    cursor.close()
+def add_event (): # Add row into database table and treewiev 
     def add_end_event ():
-        conn=psycopg2.connect (
-            host= "localhost",          #way to your database
-            database="xxx",             #name your databese
-            user="user",                
-            password="your_passworld"
-        )
         description_get=field_description.get()
         value_get=field_value.get()
-        cursor=conn.cursor ()
+        link_up()
         cursor.execute (            
-            "INSERT INTO your_sql_table_name (description_data,value_data,create_time) VALUES (%s,%s,NOW())",(description_get,value_get)
+            "INSERT INTO "+db_table(db_columns)+" VALUES (%s,%s,NOW())",(description_get,value_get)
         ) #description_data,value_data,create_time - names of columns in yor database table - your_sql_table_name
         conn.commit ()
-        cursor.close()
-        conn.close()
-        print(conn.closed)
+        link_down()
         field_description.destroy ()
         field_value.destroy ()
         button_ready.destroy ()
-        
     field_description = tk.Entry (frame_two,width=40,textvariable=tk.StringVar)
     field_value = tk.Entry (frame_two,width=15,textvariable=tk.StringVar)
     button_ready = tk.Button (frame_two,text=('Hotovo'),background='#59c919',command=add_end_event)
     field_description.grid (column=1,row=2,sticky='n')
     field_value.grid (column=3,row=2,sticky='n')
     button_ready.grid (column=4,row=2,sticky='n')
-
-def repair_event():
+def repair_event(): # Update/repair row in database table and treewiev
     def repair_end_event():
-        conn=psycopg2.connect (
-            host= "localhost",          #way to your database
-            database="xxx",             #name your databese
-            user="user",                
-            password="your_passworld"
-        )
         selected_item = tree.focus()
         column_value=tree.item(selected_item,)['values']
-        print (selected_item)
         description_get=field_description.get()
         value_get=field_value.get()
         tree.item(selected_item,values=(description_get,value_get,column_value[2]))
-        cursor=conn.cursor ()
+        link_up()
         cursor.execute (
-            "UPDATE your_sql_table_name SET description_data=%s,value_data=%s WHERE description_data=%s AND value_data=%s AND create_time=%s",(description_get,value_get,column_value[0],column_value[1],column_value[2])
+            "UPDATE "+db_table+" SET description=%s,value_data=%s WHERE description=%s AND value_data=%s AND c_time=%s",(description_get,value_get,column_value[0],column_value[1],column_value[2])
         ) #description_data,value_data,create_time - names of columns in yor database table - your_sql_table_name
         conn.commit ()
-        cursor.close()
-        conn.close()
-        print(conn.closed)
+        link_down()
         field_description.destroy ()
         field_value.destroy ()
         button_ready.destroy ()
-        
     field_description = tk.Entry (frame_two, width=40,textvariable=tk.StringVar)
     field_value = tk.Entry (frame_two,width=15,textvariable=tk.StringVar)
     button_ready = tk.Button (frame_two,text=('Hotovo'),background='#59c919',command=repair_end_event)
     field_description.grid (column=1,row=2,sticky='n')
     field_value.grid (column=3,row=2,sticky='n')
     button_ready.grid (column=4,row=2,sticky='n')
-
-def delete_event():
-    conn=psycopg2.connect (
-            host= "localhost",          #way to your database
-            database="xxx",             #name your databese
-            user="user",                
-            password="your_passworld"
-        )
+def delete_event(): # Remove row from database table and treewiev
     selected_item = tree.focus()
     column_value=tree.item(selected_item,)['values']
     tree.delete(selected_item)
-    print (selected_item)
-    cursor=conn.cursor ()
+    link_up()
     cursor.execute (
-        "DELETE FROM your_sql_table_name WHERE description_data=%s AND value_data=%s AND create_time=%s",(column_value[0],column_value[1],column_value[2])
+        "DELETE FROM "+db_table+" WHERE description=%s AND value_data=%s AND c_time=%s",(column_value[0],column_value[1],column_value[2])
     ) #description_data,value_data,create_time - names of columns in yor database table - your_sql_table_name
     conn.commit ()
-    cursor.close()
-    conn.close()
-    print(conn.closed)
+    link_down()
 
-conn=psycopg2.connect (
-        host= "localhost",          #way to your database
-        database="xxx",             #name your databese
-        user="user",                
-        password="your_passworld"
-    )
-cursor=conn.cursor ()
-cursor.execute("SELECT * FROM your_sql_table_name ;")
-vysledky=cursor.fetchall()
+#First select form table
+link_up()
+cursor.execute("SELECT * FROM "+db_table)
+rows=cursor.fetchall()
+link_down()
+#Create and setup treewiev
 tree = ttk.Treeview(frame_three, column=("c1", "c2", "c3"), show='headings')
-tree.column("#1", anchor=tk.CENTER,)
-tree.heading("#1", text="Description")
-tree.column("#2", anchor=tk.CENTER)
-tree.heading("#2", text="Value")
-tree.column("#3", anchor=tk.CENTER)
-tree.heading("#3", text="Date")
+tree.column("c1", anchor=tk.CENTER,)
+tree.heading("c1", text="Description")
+tree.column("c2", anchor=tk.CENTER)
+tree.heading("c2", text="Value")
+tree.column("c3", anchor=tk.CENTER)
+tree.heading("c3", text="Date")
 tree.tag_configure('positive',background='#76ff76')
 tree.tag_configure('negative', background='#f66')
 tree.grid(column=0,row=0)
-for row in vysledky:
+#Make tag for any row in treewiev - if row in column2 is negative value intiger/float, row will have red background
+for row in rows:
     state_value = 'positive' if row[1] >=0 else 'negative'
     tree.insert ("",tk.END,values=row,tags=state_value)
-cursor.close ()
-conn.close ()
-
+#Create buttons for functions
 button_add= tk.Button (frame_one,text=('Add'),background='#59c919',width=6,height=3, command=add_event)
 button_add.grid (column=0,row=0,padx=5,pady=5,ipadx=3)
-button_add= tk.Button (frame_one,text=('Repair'),background='#59c919',width=6,height=3, command=repair_event)
-button_add.grid (column=1,row=0,padx=5,pady=5,ipadx=3)
-button_add= tk.Button (frame_one,text=('Delete'),background='#59c919',width=6,height=3, command=delete_event)
-button_add.grid (column=2,row=0,padx=5,pady=5,ipadx=3)
-
-print(conn.closed)
+button_repair= tk.Button (frame_one,text=('Repair'),background='#59c919',width=6,height=3, command=repair_event)
+button_repair.grid (column=1,row=0,padx=5,pady=5,ipadx=3)
+button_delete= tk.Button (frame_one,text=('Delete'),background='#59c919',width=6,height=3, command=delete_event)
+button_delete.grid (column=2,row=0,padx=5,pady=5,ipadx=3)
 
 frame_main.mainloop ()
